bac646eb0570b5d363c118d213e3020cec934460
Oscar Guzman <o1speed1@hotmail.com>
2025-01-24 22:42:17 -0500

    test if that the env

--- web/app/routes/login.tsx
+++ web/app/routes/login.tsx
@@ -7,12 +7,15 @@ declare global {
 }
 
 export default function Login() {
+  console.log({ env: window.ENV });
+  const authUrl = window?.ENV?.authUrl || "";
+
   return (
     <>
       <div className="flex flex-col items-center justify-center mb-8">
         <h2>Login</h2>
         <a
-          href={`${window.ENV.authUrl}/auth/google`}
+          href={`${authUrl}/auth/google`}
           className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600"
         >
           Login with google
