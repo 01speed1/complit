03a70de991760d420f3ef4812217ccd470394ee3
Oscar Guzman <o1speed1@hotmail.com>
2025-01-24 22:28:49 -0500

    change algorithm

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -34,7 +34,9 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.email, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, {
+        algorithm: 'HS256',
+      }),
     };
   }
 
